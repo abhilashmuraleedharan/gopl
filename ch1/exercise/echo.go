f465eced3ae377ed79c1767de081a16dfc323069
Abhilash Muraleedharan <abhilashmuraleedharan@outlook.com>
2020-05-09 20:42:35 +0530

    Add echo exercise program

---
+++ ch1/exercise/echo.go
@@ -0,0 +1,14 @@
+// Modify the echo program to print the index and value of each of its arguments, one per line
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	for index, arg := range os.Args[1:] {
+		fmt.Println(strconv.Itoa(int(index)) + " " + arg)
+	}
+}
