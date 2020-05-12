a74e84505bd090b67e0cf2231a3c08775cbf8f7f
Abhilash Muraleedharan <abhilashmuraleedharan@outlook.com>
2020-05-12 13:33:25 +0530

    chore: Update programs with useful comments

--- ch1/dup1/dup.go
+++ ch1/dup1/dup.go
@@ -9,12 +9,20 @@ import (
 )
 
 func main() {
+	// Make an empty map that takes string as key and int as value
 	contents := make(map[string]int)
+	// NewScanner function of bufio package produces a scanner type object
+	// that reads given input and breaks it into lines or words
 	input := bufio.NewScanner(os.Stdin)
-	for input.Scan() {
-		contents[input.Text()]++
+	for input.Scan() { // Each call to input.Scan() reads the next line and removes \n from end
+		if len(input.Text()) > 0 { // Exit the program if an empty line is entered
+			contents[input.Text()]++ // input.Text() retrieves the scanned value
+		} else {
+			break
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
+
 	for line, n := range contents {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
--- ch1/echo3/echo.go
+++ ch1/echo3/echo.go
@@ -8,6 +8,9 @@ import (
 )
 
 func main() {
+	// Join concatenates the elements of its first argument to create a single string.
+	// The separator string sep is placed between elements in the resulting string.
+
 	fmt.Println(strings.Join(os.Args[1:], " ")) // More efficient than the previous variants
-	// fmt.Println(os.Args[1:])                    // Any slice may be printed this way
+	// fmt.Println(os.Args[1:])                 // Any slice may be printed this way
 }
